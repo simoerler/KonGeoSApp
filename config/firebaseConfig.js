fc90e91e9e85bf7adbd40deced8752df768bafbc
Niiicih <108759960+Niiicih@users.noreply.github.com>
2022-07-12 20:49:02 +0200

    Add files via upload

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -1,13 +1,22 @@
-const firebaseConfig = {
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getDatabase } from "firebase/database";
 
-    apiKey: "AIzaSyD5R2HX0e5Z0P9JAIi_jVMz8ZUE5jj-jfk",
-    authDomain: "igsm-88697.firebaseapp.com",
-    databaseURL: "https://igsm-88697.firebaseio.com",
-    projectId: "igsm-88697",
-    storageBucket: "igsm-88697.appspot.com",
-    messagingSenderId: "618078582463",
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAVZsgWypsPM6GvCd2kLBm9TbC9RGPiee4",
+    authDomain: "kongeosapp.firebaseapp.com",
+    projectId: "kongeosapp",
+    storageBucket: "kongeosapp.appspot.com",
+    messagingSenderId: "842006962157",
+    appId: "1:842006962157:web:259cc19385fbd4cad5d1e4",
+    databaseURL: "https://console.firebase.google.com/project/kongeosapp/database/kongeosapp-default-rtdb/data/~2F",
+    measurementId: "G-EYMGWZJ3WC"
 };
 
 export default firebaseConfig;
-  
-
