f3dec1659882fd5c17fe44b8d8d3c0a88b393f95
Simoerler <simone.goerler@gmail.com>
2022-08-19 22:46:39 +0200

    fix some things for notifications hopefully

--- config/registerForPushNotifications.js
+++ config/registerForPushNotifications.js
@@ -1,11 +1,10 @@
 import * as Notifications from 'expo-notifications';
-import * as Permissions from 'expo-permissions';
 import * as firebase from "firebase";
+import NotificationsScreen from '../screens/NotificationsScreen';
 
 export async function registerForPushNotificationsAsync() {
-    const {status: existingStatus} = await Permissions.getAsync(
-        Permissions.NOTIFICATIONS
-    );
+    console.log("I'm registering");
+    const {status: existingStatus} = await Notifications.requestPermissionsAsync();
     let finalStatus = existingStatus;
 
     // only ask if permissions have not already been determined, because
@@ -13,9 +12,11 @@ export async function registerForPushNotificationsAsync() {
     if (existingStatus !== 'granted') {
         // Android remote notification permissions are granted during the app
         // install, so this will only ask on iOS
-        const {status} = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+        const {status} = await Notifications.getPermissionsAsync();//Permissions.askAsync(Permissions.NOTIFICATIONS);
         finalStatus = status;
+        console.log("PERMISSION IS NOT GRANTED");
     }
+    console.log("PERMISSION IS GRANTED");
 
     // Stop here if the user did not grant permissions
     if (finalStatus !== 'granted') {
@@ -24,7 +25,7 @@ export async function registerForPushNotificationsAsync() {
 
     // Get the token that uniquely identifies this device
     const token = await Notifications.getExpoPushTokenAsync();
-    const tokenNumber = token.slice(18, 40);
+    const tokenNumber = token.data.slice(18, 40);
     // POST the token to our backend so we can use it to send pushes from there
     const updates = {};
     updates['/' + tokenNumber] = token;
