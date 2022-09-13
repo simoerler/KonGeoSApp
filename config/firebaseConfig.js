9ff37e941363148f7ecbafc0e61ce988caf8ec28
Simoerler <simone.goerler@gmail.com>
2022-09-13 20:21:28 +0200

    change to new firebase

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -5,14 +5,14 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAVZsgWypsPM6GvCd2kLBm9TbC9RGPiee4",
-    authDomain: "kongeosapp.firebaseapp.com",
-    projectId: "kongeosapp",
-    storageBucket: "kongeosapp.appspot.com",
-    messagingSenderId: "842006962157",
-    appId: "1:842006962157:web:259cc19385fbd4cad5d1e4",
-    databaseURL: "https://kongeosapp-default-rtdb.europe-west1.firebasedatabase.app",
-    measurementId: "G-EYMGWZJ3WC"
+    apiKey: "AIzaSyDOGzxIzS1UZMMk10ZCm5BVnV42qwFWVns",
+    authDomain: "kongeosapp-e4fb7.firebaseapp.com",
+    projectId: "kongeosapp-e4fb7",
+    storageBucket: "kongeosapp-e4fb7.appspot.com",
+    messagingSenderId: "252504446726",
+    appId: "1:252504446726:web:1351977bf42f9452a2b087",
+    databaseURL: "https://kongeosapp-e4fb7-default-rtdb.europe-west1.firebasedatabase.app/",
+    //measurementId: "G-EYMGWZJ3WC"
 };
 
 export default firebaseConfig;
